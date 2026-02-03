727cc56a18233b5a785dc4cb34d42f919b052803
mm4351474 <mm4351474@gmail.com>
2026-02-03 13:11:36 +0530

    Add VIBGYOR color switch case program

---
+++ vibgyorusingswitch.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main(){
+	char a;
+	printf("Enter a letter in VIBGYOR : "); 
+	scanf("%c",&a);
+	
+	switch(a)
+	{
+		case 'v':
+		case 'V': 
+			printf("Violet💜️\n");
+			break;
+		case 'i' : 
+		case 'I' :
+			printf("Indigo🫐️\n");
+			break;
+		case 'b' : 
+		case 'B' : 
+			printf("Blue💙️\n");
+			break;
+		case 'g' : 
+		case 'G' : 
+			printf("Green💚️\n");
+			break;
+		case 'y' : 
+		case 'Y' : 
+			printf("Yellow💛️\n");
+			break;
+		case 'o' : 
+		case 'O' : 
+			printf("Orange🧡️\n");
+			break;
+		case 'r' : 
+		case 'R' : 
+			printf("Red❤️\n");
+			break;
+		default : 
+			printf("This is NOT in rainbow 🥀️\n");
+	}
+	return 0;
+}
