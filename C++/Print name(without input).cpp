c0b31de67c902f633c0087a1229125c64df9317a
MIDHUN MADHAV PM <mm4351474@gmail.com>
2026-04-03 00:04:18 +0530

    Add program to print name and address

---
+++ C++/Print name(without input).cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"My name is Midhun Madhav PM\n My address is Thrissur ,Kerala ,India ,Asia ,world, earth,solar system ,milky way....."<<endl;
+}
