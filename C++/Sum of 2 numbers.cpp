dd6f70b6ff102fad5874cc8d1160c26ac11fab07
MIDHUN MADHAV PM <mm4351474@gmail.com>
2026-04-03 00:09:04 +0530

    Add C++ program to sum two numbers

---
+++ C++/Sum of 2 numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,s;
+    cout<<"Enter First number a = ";
+    cin>>a;
+    cout<<"Enter second number b = ";
+    cin>>b;
+    s = a + b;
+    cout<<"sum s = "<<s<<endl;
+    return 0;
+}
